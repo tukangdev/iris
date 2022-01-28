19edd0a5876a050fe2af3c9bf8fbad3311898029
Amin Roslan <amnrsln@gmail.com>
2022-01-28 17:10:32 +0800

    add some features

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import {
-  faPlay,
-  faRecordVinyl,
+  faRedo,
   faSave,
   faStop,
   faVideo,
@@ -15,6 +14,20 @@ function App() {
   const mediaRecorderRef = React.useRef<any>(null);
   const [capturing, setCapturing] = React.useState(false);
   const [recordedChunks, setRecordedChunks] = React.useState([]);
+  const [timeRecording, setTimeRecording] = React.useState(0);
+  const increment = React.useRef<any>(null);
+
+  const handleStart = () => {
+    setCapturing(true);
+    increment.current = setInterval(() => {
+      setTimeRecording((timer) => timer + 1);
+    }, 1000);
+  };
+
+  const handleReset = () => {
+    clearInterval(increment.current);
+    setTimeRecording(0);
+  };
 
   const handleDataAvailable = React.useCallback(
     ({ data }) => {
@@ -33,6 +46,9 @@ function App() {
 
   const handleStartCaptureClick = React.useCallback(() => {
     setCapturing(true);
+    increment.current = setInterval(() => {
+      setTimeRecording((timer) => timer + 1);
+    }, 1000);
     mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
       mimeType: "video/webm",
     });
@@ -45,10 +61,12 @@ function App() {
 
   const handleStopCaptureClick = React.useCallback(() => {
     mediaRecorderRef.current.stop();
+    clearInterval(increment.current);
     setCapturing(false);
   }, [mediaRecorderRef, setCapturing]);
 
   const handleDownload = React.useCallback(() => {
+    setTimeRecording(0);
     if (recordedChunks.length) {
       const blob = new Blob(recordedChunks, {
         type: "video/webm",
@@ -61,6 +79,13 @@ function App() {
       a.download = "react-webcam-stream-capture.webm";
       a.click();
       window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  const handleResetCaptureClick = React.useCallback(() => {
+    setTimeRecording(0);
+    if (recordedChunks.length) {
       setRecordedChunks([]);
     }
   }, [recordedChunks]);
@@ -100,6 +125,20 @@ function App() {
     );
   };
 
+  const ResetButton = ({ disabled }: { disabled: boolean }) => {
+    return (
+      <button onClick={handleResetCaptureClick} disabled={disabled}>
+        <div
+          className={`h-16 w-16 ${
+            disabled ? "bg-gray-500" : "bg-blue-500"
+          } rounded-full text-white text-2xl flex justify-center items-center`}
+        >
+          <FontAwesomeIcon icon={faRedo} />
+        </div>
+      </button>
+    );
+  };
+
   return (
     <div>
       <Helmet>
@@ -116,11 +155,19 @@ function App() {
           videoConstraints={videoConstraints}
           ref={webcamRef}
         />
-        <div className="z-10 absolute bottom-10">
+        <div className="z-10 absolute bottom-10 bg-white py-2 px-6 rounded-md w-96 flex flex-row justify-evenly items-center gap-2">
           <RecordButton
             capturing={capturing}
             download={recordedChunks.length > 0}
           />
+          <ResetButton disabled={recordedChunks.length === 0} />
+          <p className="font-thin text-2xl text-gray-500">
+            <span>{("0" + Math.floor(timeRecording / 3600)).slice(-2)}:</span>
+            <span>
+              {("0" + Math.floor((timeRecording / 60) % 60)).slice(-2)}:
+            </span>
+            <span>{("0" + (timeRecording % 60)).slice(-2)}</span>
+          </p>
         </div>
       </div>
     </div>
