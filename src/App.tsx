6aa5472416a3447f84b2511376d678dbd9dcbd8e
Amin Roslan <amnrsln@gmail.com>
2022-01-28 16:27:43 +0800

    Initial commits

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,128 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  faPlay,
+  faRecordVinyl,
+  faSave,
+  faStop,
+  faVideo,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import React from "react";
+import { Helmet } from "react-helmet";
+import Webcam from "react-webcam";
 
 function App() {
+  const webcamRef = React.useRef<any>(null);
+  const mediaRecorderRef = React.useRef<any>(null);
+  const [capturing, setCapturing] = React.useState(false);
+  const [recordedChunks, setRecordedChunks] = React.useState([]);
+
+  const handleDataAvailable = React.useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const videoConstraints = {
+    width: 1280,
+    height: 720,
+    facingMode: "user",
+  };
+
+  const handleStartCaptureClick = React.useCallback(() => {
+    setCapturing(true);
+    mediaRecorderRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: "video/webm",
+    });
+    mediaRecorderRef.current.addEventListener(
+      "dataavailable",
+      handleDataAvailable
+    );
+    mediaRecorderRef.current.start();
+  }, [webcamRef, setCapturing, mediaRecorderRef, handleDataAvailable]);
+
+  const handleStopCaptureClick = React.useCallback(() => {
+    mediaRecorderRef.current.stop();
+    setCapturing(false);
+  }, [mediaRecorderRef, setCapturing]);
+
+  const handleDownload = React.useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, {
+        type: "video/webm",
+      });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      document.body.appendChild(a);
+      a.setAttribute("style", "display: none");
+      a.href = url;
+      a.download = "react-webcam-stream-capture.webm";
+      a.click();
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  const RecordButton = ({
+    capturing,
+    download,
+  }: {
+    capturing: boolean;
+    download: boolean;
+  }) => {
+    if (download) {
+      return (
+        <button onClick={handleDownload}>
+          <div className="h-16 w-16 bg-red-500 rounded-full text-white text-2xl flex justify-center items-center">
+            <FontAwesomeIcon icon={faSave} />
+          </div>
+        </button>
+      );
+    }
+
+    if (capturing) {
+      return (
+        <button onClick={handleStopCaptureClick}>
+          <div className="h-16 w-16 bg-red-500 rounded-full text-white text-2xl flex justify-center items-center">
+            <FontAwesomeIcon icon={faStop} />
+          </div>
+        </button>
+      );
+    }
+    return (
+      <button onClick={handleStartCaptureClick}>
+        <div className="h-16 w-16 bg-red-500 rounded-full text-white text-2xl flex justify-center items-center">
+          <FontAwesomeIcon icon={faVideo} />
+        </div>
+      </button>
+    );
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Iris</title>
+      </Helmet>
+      <div
+        className="w-screen h-screen bg-gray-900 flex justify-center items-center flex-col"
+        id="video-stream"
+      >
+        <Webcam
+          audio
+          muted
+          videoConstraints={videoConstraints}
+          ref={webcamRef}
+        />
+        <div className="z-10 absolute bottom-10">
+          <RecordButton
+            capturing={capturing}
+            download={recordedChunks.length > 0}
+          />
+        </div>
+      </div>
     </div>
   );
 }
